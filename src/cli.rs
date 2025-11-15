9cb3ca17cfe35e1d9e263efb29ee38f7cd65536b
onichandame <zxinmyth@gmail.com>
2025-11-16 03:12:25 +0800

    fix

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,12 @@
 #[derive(clap::Parser)]
 #[command(version, about)]
 pub(crate) struct Cli {
-    #[arg(long)]
+    #[arg(long, env)]
     pub(crate) home_server: String,
-    #[arg(long)]
+    #[arg(long, env)]
     pub(crate) username: String,
-    #[arg(long)]
+    #[arg(long, env)]
     pub(crate) password: String,
-    #[arg(long)]
+    #[arg(long, env)]
     pub(crate) room_id: String,
 }
